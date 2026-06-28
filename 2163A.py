13621e7dbeb4af379d7b3024306731da6841e31e
Awel Surur <awelsurur69@gmail.com>
2026-06-28 17:59:06 +0300

    Create 2163A.py

---
+++ 2163A.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    a.sort()
+
+    ok = True
+    for i in range(1, n - 1, 2):
+        if a[i] != a[i + 1]:
+            ok = False
+            break
+
+    print("YES" if ok else "NO")
