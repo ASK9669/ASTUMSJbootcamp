1a14ffcc9f646e978efe4e1c221635fcfd386e65
Awel Surur <awelsurur69@gmail.com>
2026-07-17 23:39:32 +0300

    Implement hotelier seating logic in 1200A_Hotelier.py

---
+++ 1200A_Hotelier.py
@@ -0,0 +1,16 @@
+n = int(input())
+s = list(input())
+lst = ["0"]*10
+for i in range(n):
+    if s[i] == 'L':
+        left= lst.index("0")
+        lst[left] = "1"
+    elif s[i] == 'R':
+        right = 9-lst[::-1].index("0")
+        lst[right] = "1"
+    else:
+        x_index =int(s[i])
+        lst[x_index] = "0"
+        
+        
+print("".join(lst))
